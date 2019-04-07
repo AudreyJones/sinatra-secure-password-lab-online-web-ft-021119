9cdf5ea89ce766e23c98bdb9fab122181932dddf
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-07 22:15:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/failure"
+    else
   end
 
   get "/failure" do
